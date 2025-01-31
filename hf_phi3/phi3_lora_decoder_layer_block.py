3b6d7c9244c93fbe84d9fd2612e8a389e00778d3
Kevin Stephano <kevin.stephano@gmail.com>
2025-01-30 23:00:18 -0800

    Refactor phi3 files.

--- hf_phi3/phi3_lora_decoder_layer_block.py
+++ hf_phi3/phi3_lora_decoder_layer_block.py
@@ -1,21 +1,11 @@
-import torch
-from torch import nn
-import sys
-from thunder_model_blocks.utils import runner
-from thunder_model_blocks.utils.lora import patch_linear_module
-#from nemo.collections.llm.peft.lora import patch_linear_module
 
-from transformers import AutoConfig
+import sys
+import torch
+
+from thunder_model_blocks.utils import runner
+from thunder_model_blocks.hf_phi3 import phi3_config
 from transformers.cache_utils import DynamicCache
 from transformers.models.phi3.modeling_phi3 import Phi3DecoderLayer
-
-config = AutoConfig.from_pretrained("microsoft/Phi-3.5-mini-instruct")
-
-config.batch_size = 1
-config.seq_len = 8192
-config._attn_implementation = "sdpa"
-configs = {}
-configs[config.name_or_path] = config
 
 class MyModel(torch.nn.Module):
     def __init__(self, config):
@@ -40,43 +30,30 @@ class MyModel(torch.nn.Module):
                          **kwargs
                          )
         return out
-"""
-hidden_states torch.Size([1, 8192, 3072]) (25165824, 3072, 1) torch.bfloat16
-position_ids torch.Size([8192]) (1,) torch.int64
-DynamicCache()
-True
-cache_position torch.Size([8192]) (1,) torch.int64
-position_embeddings[0] torch.Size([1, 8192, 96]) (786432, 96, 1) torch.bfloat16
-position_embeddings[1] torch.Size([1, 8192, 96]) (786432, 96, 1) torch.bfloat16
-kwargs {}
-"""
 
 if __name__ == "__main__":
+    config = phi3_config.config()
+    config.lora = True
+    configs = {config.name_or_path: config}
+
     for name,cfg in configs.items():
         attn_hidden_size = cfg.hidden_size // cfg.num_attention_heads
-        print("attn_hidden_size", attn_hidden_size)
-        def inputs():
-            hidden_states = torch.randn(cfg.batch_size, cfg.seq_len, cfg.hidden_size, device='cuda', dtype=torch.bfloat16, requires_grad=True)
+        def inputs(dtype, batch_size=cfg.batch_size, seq_len=cfg.seq_len):
+            hidden_states = torch.randn(batch_size, seq_len, cfg.hidden_size, device='cuda', dtype=dtype, requires_grad=True)
             position_embeddings = (
-                    torch.randn(cfg.batch_size, cfg.seq_len, attn_hidden_size, device='cuda', dtype=torch.bfloat16, requires_grad=False),
-                    torch.randn(cfg.batch_size, cfg.seq_len, attn_hidden_size, device='cuda', dtype=torch.bfloat16, requires_grad=False),
+                    torch.randn(batch_size, seq_len, attn_hidden_size, device='cuda', dtype=dtype, requires_grad=False),
+                    torch.randn(batch_size, seq_len, attn_hidden_size, device='cuda', dtype=dtype, requires_grad=False),
                     )
-            cache_position = torch.arange(cfg.seq_len, device='cuda')
-            position_ids = torch.arange(cfg.seq_len, device='cuda')
+            cache_position = torch.arange(seq_len, device='cuda')
+            position_ids = torch.arange(seq_len, device='cuda')
             return {"hidden_states": hidden_states,
                     "position_embeddings": position_embeddings,
                     "cache_position": cache_position,
                     "position_ids": position_ids,
                     }
         
-        def grads():
-            grad = torch.randn(cfg.batch_size, cfg.seq_len, cfg.hidden_size, device='cuda', dtype=torch.bfloat16, requires_grad=False)
+        def grads(dtype, batch_size=cfg.batch_size, seq_len=cfg.seq_len):
+            grad = torch.randn(batch_size, seq_len, cfg.hidden_size, device='cuda', dtype=dtype, requires_grad=False)
             return grad
 
-        model = MyModel(cfg)
-        for module in model.modules():
-            if isinstance(module, nn.Linear):
-                module = patch_linear_module(module, dropout=0.0)
-        model = model.cuda().bfloat16()
-        #print(model)
-        runner.run(sys.argv, name, cfg.batch_size, cfg.seq_len, model, inputs, False, grads)
+        runner.run(sys.argv, name, cfg, MyModel, inputs, module_has_loss=False, grad_fn=grads)
--- hf_phi3/phi3_lora_mlp_block.py
+++ hf_phi3/phi3_lora_mlp_block.py
@@ -1,21 +1,9 @@
-from collections import OrderedDict
-import torch
-from torch import nn
 import sys
+import torch
+
 from thunder_model_blocks.utils import runner
-from thunder_model_blocks.utils.lora import patch_linear_module
-#from nemo.collections.llm.peft.lora import patch_linear_module
-
-from transformers import AutoConfig
+from thunder_model_blocks.hf_phi3 import phi3_config
 from transformers.models.phi3.modeling_phi3 import Phi3MLP
-
-config = AutoConfig.from_pretrained("microsoft/Phi-3.5-mini-instruct")
-
-config.batch_size = 1
-config.seq_len = 8192
-config._attn_implementation = "sdpa"
-configs = {}
-configs[config.name_or_path] = config
 
 class MyModel(torch.nn.Module):
     def __init__(self, config):
@@ -27,18 +15,16 @@ class MyModel(torch.nn.Module):
         return (out,)
 
 if __name__ == "__main__":
+    config = phi3_config.config()
+    config.lora = True
+    configs = {config.name_or_path: config}
+
     for name,cfg in configs.items():
-        def inputs():
-            hidden_states = torch.randn(cfg.batch_size, cfg.seq_len, cfg.hidden_size, device='cuda', dtype=torch.bfloat16, requires_grad=True)
+        def inputs(dtype, batch_size=cfg.batch_size, seq_len=cfg.seq_len):
+            hidden_states = torch.randn(batch_size, seq_len, cfg.hidden_size, device='cuda', dtype=dtype, requires_grad=True)
             return {"hidden_states": hidden_states}
-        def grads():
-            grad = torch.randn(cfg.batch_size, cfg.seq_len, cfg.hidden_size, device='cuda', dtype=torch.bfloat16, requires_grad=False)
+        def grads(dtype, batch_size=cfg.batch_size, seq_len=cfg.seq_len):
+            grad = torch.randn(batch_size, seq_len, cfg.hidden_size, device='cuda', dtype=dtype, requires_grad=False)
             return grad
 
-        model = MyModel(cfg)
-        for module in model.modules():
-            if isinstance(module, nn.Linear):
-                module = patch_linear_module(module, dropout=0)
-        model = model.cuda().bfloat16()
-        #print(model)
-        runner.run(sys.argv, name, cfg.batch_size, cfg.seq_len, model, inputs, False, grads)
+        runner.run(sys.argv, name, cfg, MyModel, inputs, module_has_loss=False, grad_fn=grads)
