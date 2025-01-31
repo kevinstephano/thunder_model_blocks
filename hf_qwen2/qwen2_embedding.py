66d137beb662178ef3484f523fb79091b72185a4
Kevin Stephano <kevin.stephano@gmail.com>
2025-01-30 22:14:02 -0800

    Refactor the rest of qwen2.

--- hf_qwen2/qwen2_embedding.py
+++ hf_qwen2/qwen2_embedding.py
@@ -1,45 +1,31 @@
-import torch
-from torch import nn
 import sys
+import torch
+
 from thunder_model_blocks.utils import runner
-from thunder_model_blocks.utils.lora import patch_linear_module
-#from nemo.collections.llm.peft.lora import patch_linear_module
-
-from transformers import AutoConfig
-from transformers.models.qwen2 import Qwen2PreTrainedModel
-
-config = AutoConfig.from_pretrained("Qwen/Qwen2.5-7B-Instruct")
-
-config.batch_size = 1
-config.seq_len = 4096
-config._attn_implementation = "sdpa"
-configs = {}
-configs[config.name_or_path] = config
+from thunder_model_blocks.hf_qwen2 import qwen2_config
+from transformers.models.qwen2.modeling_qwen2 import Qwen2PreTrainedModel
 
 class MyModel(Qwen2PreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
-        self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, config.pad_token_id)
+        self.embed_tokens = torch.nn.Embedding(config.vocab_size, config.hidden_size, config.pad_token_id)
         # Initialize weights and apply final processing
         self.post_init()
 
-    def forward(self, input_ids: torch.LongTensor = None):
+    def forward(self, input_ids: torch.LongTensor):
         inputs_embeds = self.embed_tokens(input_ids)
         return (inputs_embeds,)
 
 if __name__ == "__main__":
+    config = qwen2_config.config()
+    configs = {config.name_or_path: config}
+
     for name,cfg in configs.items():
-        def inputs():
-            input_ids = torch.randint(0, cfg.vocab_size, (cfg.batch_size, cfg.seq_len), device='cuda', requires_grad=False)
+        def inputs(dtype, batch_size=cfg.batch_size, seq_len=cfg.seq_len):
+            input_ids = torch.randint(0, cfg.vocab_size, (batch_size, seq_len), device='cuda', requires_grad=False)
             return {"input_ids": input_ids}
-        def grads():
-            grad = torch.randn(cfg.batch_size, cfg.seq_len, cfg.hidden_size, device='cuda', dtype=torch.bfloat16, requires_grad=False)
+        def grads(dtype, batch_size=cfg.batch_size, seq_len=cfg.seq_len):
+            grad = torch.randn(batch_size, seq_len, cfg.hidden_size, device='cuda', dtype=dtype, requires_grad=False)
             return grad
 
-        model = MyModel(cfg)
-        for module in model.modules():
-            if isinstance(module, nn.Linear):
-                module = patch_linear_module(module, dropout=0.0)
-        model = model.cuda().bfloat16()
-        #print(model)
-        runner.run(sys.argv, name, cfg.batch_size, cfg.seq_len, model, inputs, False, grads)
+        runner.run(sys.argv, name, cfg, MyModel, inputs, module_has_loss=False, grad_fn=grads)
--- hf_qwen2/qwen2_rope.py
+++ hf_qwen2/qwen2_rope.py
@@ -1,48 +1,9 @@
-import json
-import sys
 import sys
 import torch
-from torch import nn
-from transformers.models.qwen2 import Qwen2Config
 from typing import Tuple
+
 from thunder_model_blocks.utils import runner
-
-'''
-# Example to download model and config
-from transformers import AutoConfig, AutoModel
-model = AutoModel.from_pretrained("Qwen/Qwen2.5-7B-Instruct")
-config = AutoConfig.from_pretrained("Qwen/Qwen2.5-7B-Instruct")
-'''
-
-qwen_cfg_str = r'''{
-  "_name_or_path": "Qwen/Qwen2.5-7B-Instruct",
-  "architectures": [
-    "Qwen2ForCausalLM"
-  ],
-  "attention_dropout": 0.0,
-  "bos_token_id": 151643,
-  "eos_token_id": 151645,
-  "hidden_act": "silu",
-  "hidden_size": 3584,
-  "initializer_range": 0.02,
-  "intermediate_size": 18944,
-  "max_position_embeddings": 32768,
-  "max_window_layers": 28,
-  "model_type": "qwen2",
-  "num_attention_heads": 28,
-  "num_hidden_layers": 28,
-  "num_key_value_heads": 4,
-  "rms_norm_eps": 1e-06,
-  "rope_theta": 1000000.0,
-  "sliding_window": null,
-  "tie_word_embeddings": false,
-  "torch_dtype": "bfloat16",
-  "transformers_version": "4.43.3",
-  "use_cache": true,
-  "use_sliding_window": false,
-  "vocab_size": 152064
-}
-'''
+from thunder_model_blocks.hf_qwen2 import qwen2_config
 
 def rotate_half(x):
     """Rotates half the hidden dims of the input."""
@@ -51,25 +12,6 @@ def rotate_half(x):
     return torch.cat((-x2, x1), dim=-1)
 
 def apply_rotary_pos_emb(q, k, cos, sin, position_ids=None, unsqueeze_dim=1):
-    """Applies Rotary Position Embedding to the query and key tensors.
-
-    Args:
-        q (`torch.Tensor`): The query tensor.
-        k (`torch.Tensor`): The key tensor.
-        cos (`torch.Tensor`): The cosine part of the rotary embedding.
-        sin (`torch.Tensor`): The sine part of the rotary embedding.
-        position_ids (`torch.Tensor`, *optional*):
-            Deprecated and unused.
-        unsqueeze_dim (`int`, *optional*, defaults to 1):
-            The 'unsqueeze_dim' argument specifies the dimension along which to unsqueeze cos[position_ids] and
-            sin[position_ids] so that they can be properly broadcasted to the dimensions of q and k. For example, note
-            that cos[position_ids] and sin[position_ids] have the shape [batch_size, seq_len, head_dim]. Then, if q and
-            k have the shape [batch_size, heads, seq_len, head_dim], then setting unsqueeze_dim=1 makes
-            cos[position_ids] and sin[position_ids] broadcastable to the shapes of q and k. Similarly, if q and k have
-            the shape [batch_size, seq_len, heads, head_dim], then set unsqueeze_dim=2.
-    Returns:
-        `tuple(torch.Tensor)` comprising of the query and key tensors rotated using the Rotary Position Embedding.
-    """
     cos = cos.unsqueeze(unsqueeze_dim)
     sin = sin.unsqueeze(unsqueeze_dim)
     q_embed = (q * cos) + (rotate_half(q) * sin)
@@ -87,8 +29,8 @@ def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
     hidden_states = hidden_states[:, :, None, :, :].expand(batch, num_key_value_heads, n_rep, slen, head_dim)
     return hidden_states.reshape(batch, num_key_value_heads * n_rep, slen, head_dim)
 
-class Qwen2Rope(nn.Module):
-    def __init__(self, config: Qwen2Config):
+class Qwen2Rope(torch.nn.Module):
+    def __init__(self, config):
         super().__init__()
         self.config = config
 
@@ -132,27 +74,23 @@ class Qwen2Rope(nn.Module):
         value_states = repeat_kv(value_states, self.num_key_value_groups)
         return query_states, key_states, value_states
 
-qwen2_cfg = Qwen2Config.from_dict(json.loads(qwen_cfg_str))
-qwen2_cfg.batch_size = 1
-qwen2_cfg.seq_len = 4096
-configs = {}
-configs[qwen2_cfg.name_or_path] = qwen2_cfg
-
 if __name__ == "__main__":
+    config = qwen2_config.config()
+    configs = {config.name_or_path: config}
+
     for name,cfg in configs.items():
         head_dim = cfg.hidden_size // cfg.num_attention_heads
-        def inputs():
+        def inputs(dtype, batch_size=cfg.batch_size, seq_len=cfg.seq_len):
             args = {
-                "query_in_states": torch.randn(cfg.batch_size, cfg.seq_len, cfg.num_attention_heads * head_dim, device='cuda', dtype=torch.bfloat16, requires_grad=True),
-                "key_in_states": torch.randn(cfg.batch_size, cfg.seq_len, cfg.num_key_value_heads * head_dim, device='cuda', dtype=torch.bfloat16, requires_grad=True),
-                "value_in_states": torch.randn(cfg.batch_size, cfg.seq_len, cfg.num_key_value_heads * head_dim, device='cuda', dtype=torch.bfloat16, requires_grad=True),
-                "cos": torch.randn(cfg.batch_size, cfg.seq_len, head_dim, device='cuda', dtype=torch.bfloat16, requires_grad=True),
-                "sin": torch.randn(cfg.batch_size, cfg.seq_len, head_dim, device='cuda', dtype=torch.bfloat16, requires_grad=True),
+                "query_in_states": torch.randn(batch_size, seq_len, cfg.num_attention_heads * head_dim, device='cuda', dtype=dtype, requires_grad=True),
+                "key_in_states": torch.randn(batch_size, seq_len, cfg.num_key_value_heads * head_dim, device='cuda', dtype=dtype, requires_grad=True),
+                "value_in_states": torch.randn(batch_size, seq_len, cfg.num_key_value_heads * head_dim, device='cuda', dtype=dtype, requires_grad=True),
+                "cos": torch.randn(batch_size, seq_len, head_dim, device='cuda', dtype=dtype, requires_grad=True),
+                "sin": torch.randn(batch_size, seq_len, head_dim, device='cuda', dtype=dtype, requires_grad=True),
             }
             return args
-        def grads():
-            grad = torch.randn(cfg.batch_size, cfg.num_attention_heads, cfg.seq_len, head_dim, device='cuda', dtype=torch.bfloat16, requires_grad=False)
+        def grads(dtype, batch_size=cfg.batch_size, seq_len=cfg.seq_len):
+            grad = torch.randn(batch_size, cfg.num_attention_heads, seq_len, head_dim, device='cuda', dtype=dtype, requires_grad=False)
             return grad
  
-        model = Qwen2Rope(cfg).cuda().bfloat16()
-        runner.run(sys.argv, name, cfg.batch_size, cfg.seq_len, model, inputs, False, grads)
+        runner.run(sys.argv, name, cfg, Qwen2Rope, inputs, module_has_loss=False, grad_fn=grads)
