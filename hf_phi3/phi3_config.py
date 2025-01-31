969c89f8c29f1ef3c7699f1a753de38397f3d32b
Kevin Stephano <kevin.stephano@gmail.com>
2025-01-30 23:21:21 -0800

    Forget config.

---
+++ hf_phi3/phi3_config.py
@@ -0,0 +1,8 @@
+from transformers import AutoConfig
+
+def config():
+    config = AutoConfig.from_pretrained("microsoft/Phi-3.5-mini-instruct")
+    config.batch_size = 1
+    config.seq_len = 8192
+    config._attn_implementation = "sdpa"
+    return config
