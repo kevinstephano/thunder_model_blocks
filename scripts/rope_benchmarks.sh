3d30c77ba881939dc75b8d68740aa297cdc9760a
jjsjann123 <jiej@nvidia.com>
2025-02-06 11:51:21 -0800

    fixing scripts

--- scripts/rope_benchmarks.sh
+++ scripts/rope_benchmarks.sh
@@ -1,22 +1,24 @@
 #/bin/bash
 
-python ../thunder_model_blocks/hf_phi3/phi3_rope.py --seq_lens 2048 --csv
-python ../thunder_model_blocks/hf_phi3/phi3_rope.py --seq_lens 4096 --csv
-python ../thunder_model_blocks/hf_phi3/phi3_rope.py --seq_lens 8192 --csv
-python ../thunder_model_blocks/hf_phi3/phi3_rope.py --seq_lens 12288 --csv
-python ../thunder_model_blocks/hf_phi3/phi3_rope.py --seq_lens 16384 --csv
-python ../thunder_model_blocks/hf_phi3/phi3_rope.py --seq_lens 20480 --csv
+d=$(pwd)
 
-python ../thunder_model_blocks/hf_qwen2/qwen2_rope.py --seq_lens 2048 --csv
-python ../thunder_model_blocks/hf_qwen2/qwen2_rope.py --seq_lens 4096 --csv
-python ../thunder_model_blocks/hf_qwen2/qwen2_rope.py --seq_lens 8192 --csv
-python ../thunder_model_blocks/hf_qwen2/qwen2_rope.py --seq_lens 12288 --csv
-python ../thunder_model_blocks/hf_qwen2/qwen2_rope.py --seq_lens 16384 --csv
-python ../thunder_model_blocks/hf_qwen2/qwen2_rope.py --seq_lens 20480 --csv
+python $d/thunder_model_blocks/hf_phi3/phi3_rope.py --seq_lens 2048 --csv
+python $d/thunder_model_blocks/hf_phi3/phi3_rope.py --seq_lens 4096 --csv
+python $d/thunder_model_blocks/hf_phi3/phi3_rope.py --seq_lens 8192 --csv
+python $d/thunder_model_blocks/hf_phi3/phi3_rope.py --seq_lens 12288 --csv
+python $d/thunder_model_blocks/hf_phi3/phi3_rope.py --seq_lens 16384 --csv
+python $d/thunder_model_blocks/hf_phi3/phi3_rope.py --seq_lens 20480 --csv
 
-python ../thunder_model_blocks/hf_mistral/mistral_rope.py --seq_lens 2048 --csv
-python ../thunder_model_blocks/hf_mistral/mistral_rope.py --seq_lens 4096 --csv
-python ../thunder_model_blocks/hf_mistral/mistral_rope.py --seq_lens 8192 --csv
-python ../thunder_model_blocks/hf_mistral/mistral_rope.py --seq_lens 12288 --csv
-python ../thunder_model_blocks/hf_mistral/mistral_rope.py --seq_lens 16384 --csv
-python ../thunder_model_blocks/hf_mistral/mistral_rope.py --seq_lens 20480 --csv
+python $d/thunder_model_blocks/hf_qwen2/qwen2_rope.py --seq_lens 2048 --csv
+python $d/thunder_model_blocks/hf_qwen2/qwen2_rope.py --seq_lens 4096 --csv
+python $d/thunder_model_blocks/hf_qwen2/qwen2_rope.py --seq_lens 8192 --csv
+python $d/thunder_model_blocks/hf_qwen2/qwen2_rope.py --seq_lens 12288 --csv
+python $d/thunder_model_blocks/hf_qwen2/qwen2_rope.py --seq_lens 16384 --csv
+python $d/thunder_model_blocks/hf_qwen2/qwen2_rope.py --seq_lens 20480 --csv
+
+python $d/thunder_model_blocks/hf_mistral/mistral_rope.py --seq_lens 2048 --csv
+python $d/thunder_model_blocks/hf_mistral/mistral_rope.py --seq_lens 4096 --csv
+python $d/thunder_model_blocks/hf_mistral/mistral_rope.py --seq_lens 8192 --csv
+python $d/thunder_model_blocks/hf_mistral/mistral_rope.py --seq_lens 12288 --csv
+python $d/thunder_model_blocks/hf_mistral/mistral_rope.py --seq_lens 16384 --csv
+python $d/thunder_model_blocks/hf_mistral/mistral_rope.py --seq_lens 20480 --csv
