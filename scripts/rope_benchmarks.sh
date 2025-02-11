c3e45e4234d1b2da7c279e859e8339bef1ecc036
Kevin Stephano <kevin.stephano@gmail.com>
2025-02-10 18:35:12 -0800

    Added gemma2 rope.

--- scripts/rope_benchmarks.sh
+++ scripts/rope_benchmarks.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 seqlens="1024 2048 4096 8192 12288 16384 20480"
-executors="Torch-Eager Thunder-torch.compile Thunder-nvFuser"
-models=("phi3" "qwen2" "mistral")
+executors="Thunder-torch.compile Thunder-nvFuser"
+models=("phi3" "qwen2" "mistral" "gemma2")
 
 for benchmark in $models; do
   cmd="python ../${benchmark}/${benchmark}_rope.py --seq_lens ${seqlens} --execs ${executors}"
