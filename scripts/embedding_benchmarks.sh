240923b4a7bddbbc753ec2bf4db54be85badfb24
Kevin Stephano <kevin.stephano@gmail.com>
2025-02-10 18:42:39 -0800

    Fix benchmark script iteration.

--- scripts/embedding_benchmarks.sh
+++ scripts/embedding_benchmarks.sh
@@ -4,7 +4,7 @@ seqlens="1024 2048 4096 8192 12288 16384 20480"
 executors="Torch-Eager Thunder-torch.compile Thunder-nvFuser"
 models=("phi3" "qwen2" "mistral")
 
-for benchmark in $models; do
+for benchmark in ${models[@]}; do
   cmd="python ../${benchmark}/${benchmark}_embedding.py --seq_lens ${seqlens} --execs ${executors}"
   eval $cmd
 done
--- scripts/rope_benchmarks.sh
+++ scripts/rope_benchmarks.sh
@@ -4,7 +4,7 @@ seqlens="1024 2048 4096 8192 12288 16384 20480"
 executors="Thunder-torch.compile Thunder-nvFuser"
 models=("phi3" "qwen2" "mistral" "gemma2")
 
-for benchmark in $models; do
+for benchmark in ${models[@]}; do
   cmd="python ../${benchmark}/${benchmark}_rope.py --seq_lens ${seqlens} --execs ${executors}"
   eval $cmd
 done
