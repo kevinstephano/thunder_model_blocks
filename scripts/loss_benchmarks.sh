e05ea5b9dd30a7d05983a0d13b796297a661dfd8
Kevin Stephano <kevin.stephano@gmail.com>
2025-02-11 16:33:35 -0800

    Augment loss and embedding benchmarks scripts.

--- scripts/loss_benchmarks.sh
+++ scripts/loss_benchmarks.sh
@@ -2,7 +2,7 @@
 
 seqlens="1024 2048 4096 8192 12288 16384 20480"
 executors="Torch-Eager Thunder-torch.compile Thunder-nvFuser-more-ops"
-models=("phi3" "qwen2" "mistral")
+models=("phi3" "qwen2" "mistral" "gemma2" "starcoder2" "llama")
 
 for benchmark in ${models[@]}; do
   cmd="python ../${benchmark}/${benchmark}_loss.py --seq_lens ${seqlens} --execs ${executors}"
