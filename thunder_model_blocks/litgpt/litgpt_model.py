0b3159a8f6067ca92d45f5bbfe7fa5372809b81e
Kevin Stephano <kevin.stephano@gmail.com>
2025-04-12 00:02:10 -0700

    Changed up the lack of loss handling.

--- thunder_model_blocks/litgpt/litgpt_model.py
+++ thunder_model_blocks/litgpt/litgpt_model.py
@@ -35,7 +35,7 @@ class MyModel(torch.nn.Module):
 
     def forward(self, idx: torch.LongTensor) :
         out = self.model(idx=idx)
-        return (out.sum(),)
+        return (out,)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='AutoModel on top of Thunder Based Model Examples')
@@ -54,5 +54,8 @@ if __name__ == "__main__":
             "idx": torch.randint(0, cfg.vocab_size, (batch_size, seq_len), device='cuda', requires_grad=False),
         }
         return args
+    def grads(dtype, batch_size=cfg.batch_size, seq_len=cfg.seq_len):
+        grad = torch.randn(batch_size, seq_len, cfg.padded_vocab_size, device='cuda', dtype=dtype, requires_grad=False)
+        return grad
 
-    runner.run(sys_argv, cfg.name_or_path, cfg, MyModel, inputs, module_has_loss=True)
+    runner.run(sys_argv, cfg.name_or_path, cfg, MyModel, inputs, module_has_loss=False, grad_fn=grads)
