5c083b0e1fc8c408308c6b602bfd4669a36a2901
Kevin Stephano <kevin.stephano@gmail.com>
2025-05-19 12:39:43 -0700

    Fixing up loss benchmarks to only cover the loss portion.

--- thunder_model_blocks/mistral/mistral_loss.py
+++ thunder_model_blocks/mistral/mistral_loss.py
@@ -8,13 +8,11 @@ from transformers.models.mistral.modeling_mistral import MistralPreTrainedModel
 class MyModel(MistralPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
-        self.lm_head = torch.nn.Linear(config.hidden_size, config.vocab_size, bias=False)
 
         # Initialize weights and apply final processing
         self.post_init()
 
-    def forward(self, hidden_states: torch.Tensor, labels: torch.LongTensor):
-        logits = self.lm_head(hidden_states)
+    def forward(self, logits: torch.Tensor, labels: torch.LongTensor):
         loss = self.loss_function(logits=logits, labels=labels, vocab_size=self.config.vocab_size)
         return (loss,)
 
@@ -23,7 +21,7 @@ if __name__ == "__main__":
 
     def inputs(dtype, batch_size=cfg.batch_size, seq_len=cfg.seq_len, packed_seq_fn=None):
         args = {
-            "hidden_states": torch.randn(batch_size, seq_len, cfg.hidden_size, device='cuda', dtype=dtype, requires_grad=True),
+            "logits": torch.randn(batch_size, seq_len, cfg.vocab_size, device='cuda', dtype=dtype, requires_grad=True),
             "labels": torch.randint(0, cfg.vocab_size, (batch_size, seq_len), device='cuda', requires_grad=False),
         }
         return args
--- thunder_model_blocks/starcoder2/starcoder2_loss.py
+++ thunder_model_blocks/starcoder2/starcoder2_loss.py
@@ -8,13 +8,11 @@ from transformers.models.starcoder2 import Starcoder2PreTrainedModel
 class MyModel(Starcoder2PreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
-        self.lm_head = torch.nn.Linear(config.hidden_size, config.vocab_size, bias=False)
 
         # Initialize weights and apply final processing
         self.post_init()
 
-    def forward(self, hidden_states: torch.Tensor, labels: torch.LongTensor):
-        logits = self.lm_head(hidden_states)
+    def forward(self, logits: torch.Tensor, labels: torch.LongTensor):
         loss = self.loss_function(logits=logits, labels=labels, vocab_size=self.config.vocab_size)
         return (loss,)
 
@@ -22,8 +20,10 @@ if __name__ == "__main__":
     cfg = starcoder2_config.config()
 
     def inputs(dtype, batch_size=cfg.batch_size, seq_len=cfg.seq_len, packed_seq_fn=None):
-        hidden_states = torch.randn(batch_size, seq_len, cfg.hidden_size, device='cuda', dtype=dtype, requires_grad=True)
-        labels = torch.randint(0, cfg.vocab_size, (batch_size, seq_len), device='cuda', requires_grad=False)
-        return {"hidden_states": hidden_states, "labels": labels}
+        args = {
+            "logits": torch.randn(batch_size, seq_len, cfg.vocab_size, device='cuda', dtype=dtype, requires_grad=True),
+            "labels": torch.randint(0, cfg.vocab_size, (batch_size, seq_len), device='cuda', requires_grad=False),
+        }
+        return args
 
     runner.run(sys.argv, cfg.name_or_path, cfg, MyModel, inputs, module_has_loss=True)
